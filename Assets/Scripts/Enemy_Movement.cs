8a994fd68ae02310b8be0d56a3b7bcf7449284cb
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-07-16 00:33:44 +0100

    Enemy prefab spawners complete.

---
+++ Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeedX = 0.01f;
+    [SerializeField] private float moveSpeedY = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + moveSpeedX, transform.position.y + moveSpeedY, transform.position.z);
+    }
+}
