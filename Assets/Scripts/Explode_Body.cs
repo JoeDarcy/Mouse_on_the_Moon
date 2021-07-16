0d7a0f2d74755651a7762a0c8b0561bad23a97a7
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-07-16 11:42:44 +0100

    Jerry launchpad death triggers complete.

---
+++ Assets/Scripts/Explode_Body.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode_Body : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(new Vector2(0, 50), ForceMode2D.Impulse);
+    }
+
+}
---
+++ Assets/Scripts/Launchpad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launchpad : MonoBehaviour
+{
+    [SerializeField] private GameObject jerry = null;
+	[SerializeField] private bool explosionArmed = false;
+	[SerializeField] private GameObject explosion = null;
+
+
+	private void OnTriggerExit2D(Collider2D collision) {
+			explosionArmed = true;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (explosionArmed == true) {
+			explosion.SetActive(true);
+			jerry.SetActive(false);
+		}
+	}
+}
