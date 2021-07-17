7343aa59cde845603307f3d920b4a560c5cb9504
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2021-07-17 23:19:22 +0100

    All cutscene implemented. Fight scene and pre fight cutscene still need animation. All dialog outline boxes and profile pictures and frames made. Picnic cutscene complete.

---
+++ Assets/Scripts/Dialog_Controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog_Controller : MonoBehaviour
+{
+    [SerializeField] private GameObject jerry_1 = null;
+    [SerializeField] private GameObject jerry_2 = null;
+    [SerializeField] private GameObject jerry_3 = null;
+    [SerializeField] private GameObject isobelle_1 = null;
+    [SerializeField] private GameObject harriet_1 = null;
+
+
+    [SerializeField] private float timer = 0.0f;
+    [SerializeField] private float transition_1 = 0.0f;
+    [SerializeField] private float transition_2 = 0.0f;
+    [SerializeField] private float transition_3 = 0.0f;
+    [SerializeField] private float transition_4 = 0.0f;
+
+
+    private void Start() {
+        jerry_2.SetActive(false);
+        jerry_3.SetActive(false);
+        isobelle_1.SetActive(false);
+        harriet_1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timer += Time.deltaTime;
+
+        if (timer > transition_1) {
+            isobelle_1.SetActive(true);
+            jerry_1.SetActive(false);
+        }
+
+        if (timer > transition_2) {
+            harriet_1.SetActive(true);
+            isobelle_1.SetActive(false);
+        }
+
+        if (timer > transition_3) {
+            jerry_2.SetActive(true);
+            harriet_1.SetActive(false);
+        }
+
+        if (timer > transition_4) {
+            jerry_3.SetActive(true);
+            jerry_2.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Scene_Controller_End.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Controller_End : MonoBehaviour
+{
+    [SerializeField] private GameObject reunion = null;
+    [SerializeField] private GameObject hearts = null;
+    [SerializeField] private GameObject wink = null;
+
+    [SerializeField] private float timer = 0.0f;
+    [SerializeField] private float transition_1 = 0.0f;
+    [SerializeField] private float transition_2 = 0.0f;
+    [SerializeField] private float transition_3 = 0.0f;
+    [SerializeField] private float endSceneTransition = 0.0f;
+
+    [SerializeField] private Camera cam = null;
+    [SerializeField] private GameObject camAnimator = null;
+    private Animator animator = null;
+
+    private void Start() {
+        reunion.SetActive(false);
+        hearts.SetActive(false);
+        animator = camAnimator.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timer += Time.deltaTime;
+
+        if (timer > transition_1) {
+            reunion.SetActive(true);
+            hearts.SetActive(true);
+            if (cam.orthographicSize > 2) {
+                cam.orthographicSize -= 0.001f;
+                animator.enabled = true;
+            }          
+        }
+
+        if (timer > transition_2) {
+            wink.SetActive(true);
+		}
+
+        if (timer > transition_3) {
+            wink.SetActive(false);
+        }
+
+        if (timer > endSceneTransition) {
+            SceneManager.LoadScene(7);
+        }
+    }
+}
